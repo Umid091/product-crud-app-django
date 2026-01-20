fe17587dd84aa23f0ef2e1eb3f7a734f34e8de2e
Umid <umidjorayev091@gmail.com>
2026-01-20 13:56:56 +0500

    loyiha funksiyalari classlarda yozildi va o'zgartirildi

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('product/<int:product_id>/',views.detail,name='detail'),
-    path('create/', views.create, name='create_product'),
-    path('update/<int:product_id>/', views.update_product, name='update_product'),
-    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('', views.ListView.as_view(), name='index'),
+    path('product/<int:pk>/',views.DetailView.as_view(),name='detail'),
+    path('create/', views.CreateView.as_view(), name='create_product'),
+    path('update/<int:pk>/', views.UpdateView.as_view(), name='update_product'),
+    path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,52 +2,115 @@ from django.shortcuts import render, redirect
 
 from django.shortcuts import render
 from .models import Product
+from django.views import View
 
-def index(request):
-    products = Product.objects.all()
-    return render(request, 'index.html', {'products': products})
+class ListView(View):
+    def get(self,request):
+        products=Product.objects.all()
+        return render(request, 'index.html', {'products': products})
 
-def detail(request,product_id):
-    product=Product.objects.get(id=product_id)
-    return render(request, 'detail.html',{'product':product})
+class DetailView(View):
+    def get(self,request,pk):
+        product=Product.objects.filter(id=pk).first()
+        return render(request, 'detail.html',{'product':product})
 
 
-def create(request):
-    if request.method=='POST':
+class CreateView(View):
+    def get(self,request):
+        return render(request, 'create_product.html')
+
+    def post(self,request):
         name = request.POST.get('name')
         category = request.POST.get('category')
         price = request.POST.get('price')
         quantity = request.POST.get('quantity')
-        Product.objects.create(
+        product=Product.objects.create(
+
             name=name,
             category=category,
             price=price,
             quantity=quantity,
-
         )
-        return redirect('index')
-    return render(request, 'create_product.html')
-
-
-def update_product(request, product_id):
-
-    product = Product.objects.get(id=product_id)
-
-    if request.method == "POST":
-        product.name = request.POST.get('name')
-        product.category = request.POST.get('category')
-        product.price = request.POST.get('price')
-        product.quantity = request.POST.get('quantity')
-
         product.save()
         return redirect('index')
 
-    return render(request, 'update_product.html', {'product': product})
+
+class UpdateView(View):
+    def get(self, request, pk):
+
+        product = Product.objects.filter(id=pk).first()
+        # Uni HTML-ga context sifatida yuboramiz
+        return render(request, 'update_product.html', {'product': product})
+
+    def post(self, request, pk):
+        product = Product.objects.filter(id=pk).first()
+
+        if product:
+            product.name = request.POST.get('name')
+            product.category = request.POST.get('category')
+            product.price = request.POST.get('price')
+            product.quantity = request.POST.get('quantity')
+
+            product.save()
+
+        return redirect('index')
 
 
-def delete_product(request, product_id):
+class DeleteView(View):
+    def get(self, request, pk):
+        product = Product.objects.filter(id=pk).first()
+        product.delete()
 
-    product = Product.objects.get( id=product_id)
-    product.delete()
 
-    return redirect('index')
+
+    #
+
+
+# def index(request):
+#     products = Product.objects.all()
+#     return render(request, 'index.html', {'products': products})
+#
+# def detail(request,product_id):
+#     product=Product.objects.get(id=product_id)
+#     return render(request, 'detail.html',{'product':product})
+#
+#
+# def create(request):
+#     if request.method=='POST':
+#         name = request.POST.get('name')
+#         category = request.POST.get('category')
+#         price = request.POST.get('price')
+#         quantity = request.POST.get('quantity')
+#         Product.objects.create(
+#             name=name,
+#             category=category,
+#             price=price,
+#             quantity=quantity,
+#
+#         )
+#         return redirect('index')
+#     return render(request, 'create_product.html')
+#
+#
+# def update_product(request, product_id):
+#
+#     product = Product.objects.get(id=product_id)
+#
+#     if request.method == "POST":
+#         product.name = request.POST.get('name')
+#         product.category = request.POST.get('category')
+#         product.price = request.POST.get('price')
+#         product.quantity = request.POST.get('quantity')
+#
+#         product.save()
+#         return redirect('index')
+#
+#     return render(request, 'update_product.html', {'product': product})
+#
+#
+# def delete_product(request, product_id):
+#
+#     product = Product.objects.get( id=product_id)
+#     product.delete()
+#
+#     return redirect('index')
